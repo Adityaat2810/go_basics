e9a47ae1463a97631f93d6e8fd5100e0b390964c
Aditya <adityasainics10@gmail.com>
2025-12-21 16:08:26 +0530

    [basics] simple values and variables

--- basics/01_main.go
+++ basics/01_main.go
@@ -3,6 +3,38 @@ package main
 import "fmt"
 
 func main(){
-	fmt.Println("vande matram!")
-}
+	/*********** simple values *****************************/
 
+	// int
+	fmt.Println(1)
+	fmt.Println(1+2)
+
+	// string
+	fmt.Println("vande matram!")
+
+	// boolean
+	fmt.Println(true)
+	fmt.Println(false)
+
+	// float
+	fmt.Println(10.5)
+	fmt.Println(7.0 / 3.0)
+
+	/*********************** Variables *********************/
+	var name string = "raghu"
+	var name2 = "aditya"   // infer the type
+
+	var is_adult = true  // infer the type
+
+	var age int = 89
+
+	// short-hand syntax
+	name3 := "golang"  // this synatx is used when decalre and assign value at same time
+	var name4 string    // here when we only declare only (not assign) type defination is compulsary
+	name4 = "hee hee"
+
+	var price float32 = 50.3
+
+	fmt.Println(name, "  " ,name2, "  ", is_adult, "  ", age, "  ", name3, "  ", name4, "  ", price)
+
+}
